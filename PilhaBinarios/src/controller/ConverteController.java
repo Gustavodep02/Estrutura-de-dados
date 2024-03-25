0def09fc469e87875a4646e26746aa6b7d66f169
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-03-25 16:08:36 -0300

    Add files via upload

---
+++ PilhaBinarios/src/controller/ConverteController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.Pilha;
+
+public class ConverteController {
+	public ConverteController() {
+        super();
+    }
+	Pilha p = new Pilha();
+	
+	public String decToBin(int decimal) {
+		while(decimal > 0) {
+            p.push(decimal % 2);
+            decimal = decimal / 2;
+        }
+        String bin = "";
+        while(!p.isEmpty()) {
+            try {
+				bin += p.pop();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+        return bin;
+    }
+	}
+
---
+++ PilhaBinarios/src/view/Main.java
@@ -0,0 +1,19 @@
+package view;
+import controller.ConverteController;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int valor = (Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro ate 1000: ")));
+		if (valor>1000) {
+            JOptionPane.showMessageDialog(null, "Valor invalido");
+            System.exit(0);
+        }
+		ConverteController c = new ConverteController();
+		String binario = c.decToBin(valor);
+		JOptionPane.showMessageDialog(null, "Valor em binário: " + binario);
+
+	}
+
+}
