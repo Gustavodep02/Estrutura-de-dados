91401126849744dd35a1d3dad6b4b6a481e566b1
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-03-26 14:41:11 -0300

    Add files via upload

---
+++ PilhaLista3Ex04/src/controller/NPRController.java
@@ -0,0 +1,65 @@
+package controller;
+
+import javax.swing.JOptionPane;
+
+import model.Pilha;
+
+public class NPRController {
+
+	public NPRController() {
+
+	}
+
+	Pilha p = new Pilha();
+
+	public void insereValor(Pilha p, int valor) {// adiciona valor a pilha
+		String[] opcoes = new String[] { "Sim", "Não" };
+		p.push(valor);
+		int retorno = 0;
+		while (retorno == 0) {
+			retorno = JOptionPane.showOptionDialog(null, "Vc deseja digitar mais numeros?", "Mensagem de Confirmação",
+					JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+			if(retorno==0) {
+				int num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+				p.push(num);
+			}
+		}
+	}
+
+	public int npr(Pilha p, String op) {
+		int valor1 = 0;
+		int valor2 = 0;
+		int res =0;
+		if (p.size() >= 2) {
+			try {//pega os valores a realizar o calculo
+				valor1 = p.pop();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				valor2 = p.pop();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (op.equals("+")) {// realiza a soma
+
+				res = valor1 + valor2;
+			}else if(op.equals("-")) {//realiza a subtração
+				res = valor2 - valor1;
+			}else if(op.equals("*")) {//realiza a multiplicação
+                res = valor1 * valor2;
+		}else if (op.equals("/")) {//realiza a divisão
+            	res = valor2 / valor1;
+        }
+	}else {
+		JOptionPane.showMessageDialog(null, "Não há valores suficientes para realizar a operação");
+		System.exit(0);
+    }
+	
+		p.push(res);
+		return res;
+}
+	}
+
---
+++ PilhaLista3Ex04/src/view/Main.java
@@ -0,0 +1,41 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.NPRController;
+import model.Pilha;
+
+public class Main {
+
+	public static void main(String[] args) {
+		NPRController npr = new NPRController();
+		Pilha p = new Pilha();
+
+		int retorno = 0;
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Digitar um numero "));
+		npr.insereValor(p, num);
+		String op = JOptionPane.showInputDialog("Digite a operação:");
+		int res = npr.npr(p, op);
+		System.out.println(res);
+		while (retorno != 9) {
+			int opc = Integer
+					.parseInt(JOptionPane.showInputDialog("1- Digitar mais numeros\n2- Digitar operacao\n9- sair: "));
+			switch (opc) {
+			case 1:
+				num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+				npr.insereValor(p, num);
+				break;
+			case 2:
+				op = JOptionPane.showInputDialog("Digite a operação: ");
+				res = npr.npr(p, op);
+				System.out.println(res);
+				break;
+				case 9:
+					retorno = 9;
+                    break;
+			default:
+				break;
+			}
+		}
+	}
+}
