4a25d015f206e3db453ddf3f33a401ff8ae34b00
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-04-03 04:27:23 -0300

    Add files via upload

--- ChamadoSim/src/entity/ClientePrioritario.java
+++ ChamadoSim/src/entity/ClientePrioritario.java
@@ -3,4 +3,12 @@ package entity;
 public class ClientePrioritario {
 	public int numero;
 	public String nome;
+	
+	public String toString() {
+        return "Cliente Prioritario{ " +
+               // Add your fields here. For example, if you have a name field, you can do:
+               // "name=" + name +
+               "Nome: " + nome + ", " +"Numero: "+ numero +
+               " }";
+    }
 }
