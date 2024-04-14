3bd3be870050ed352da23db49373de4e0910a74c
Gustavodep02 <47760959+Gustavodep02@users.noreply.github.com>
2024-04-14 04:36:27 -0300

    Add files via upload

---
+++ FilaLista3Ex02/src/controller/TelefoneController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import model.Fila;
+
+public class TelefoneController{
+
+	public TelefoneController() {
+		
+	}
+	
+	public void insereLigacao(Fila<String> f, String numeroTelefone){
+		f.insert(numeroTelefone);
+	}
+	public void consultaLigacao(Fila<String> f) throws Exception {
+        if (f.isEmpty()) {
+            throw new Exception("Fila vazia");	
+        } else {
+        	int tamanho = f.size();
+    		for (int i = 0;i<tamanho;i++) {
+    			f.remove();
+    		}
+        }
+		
+    }
+	
+}
---
+++ FilaLista3Ex02/src/view/Principal.java
@@ -0,0 +1,37 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.TelefoneController;
+import model.Fila;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		TelefoneController tc = new TelefoneController();
+		Fila<String> f = new Fila();
+		int opc = 0;
+		while (opc != 9) {
+			opc = Integer.parseInt(JOptionPane.showInputDialog("1 - Inserir Ligação\n2 - Consultar Ligação\n9 - Sair"));
+			switch (opc) {
+			case 1:
+                String numeroTelefone = JOptionPane.showInputDialog("Digite o número do telefone");
+                tc.insereLigacao(f, numeroTelefone);
+                break;
+			case 2:
+				try {
+                    tc.consultaLigacao(f);
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
+                break;
+			case 9:
+				System.out.println("Saindo...");
+                break;
+            default:
+            	break;
+			}
+		}
+
+	}
+}
