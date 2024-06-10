1d5334fe315ef5413949229746e452f914e46a15
Gustavodep02 <gdepaulay@hotmail.com>
2024-06-09 23:53:09 -0300

    Add files via upload

---
+++ ArvoresLista2Ex03/src/view/Main.java
@@ -0,0 +1,26 @@
+package view;
+
+import entity.Arvore;
+
+public class Main {
+	public static void main(String[] args) {
+		char [] vetor = {'k','d','m','b','c','f','l','t','p','z','r'};
+		Arvore arvore = new Arvore();
+		int tamanho = vetor.length;
+		for (int i = 0; i < tamanho; i++) {
+			arvore.insertLeaf(vetor[i]);
+		}
+		try {
+			arvore.removeChild('m');
+			arvore.prefixSearch();
+			System.out.println("===================");
+			arvore.infixSearch();
+			System.out.println("===================");
+			arvore.postfixSearch();
+			arvore.search('r');
+		}catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	}
+}
