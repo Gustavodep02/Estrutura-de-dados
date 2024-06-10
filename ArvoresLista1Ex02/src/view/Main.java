42ac796fe14a5edde0426e922b5493452c6c3b38
Gustavodep02 <gdepaulay@hotmail.com>
2024-06-09 23:13:01 -0300

    Add files via upload

---
+++ ArvoresLista1Ex02/src/view/Main.java
@@ -0,0 +1,24 @@
+package view;
+
+import entity.Arvore;
+
+public class Main {
+	public static void main(String[] args) {
+		int [] vetor = {33,15,41,38,57,34,49,43};
+		Arvore arvore = new Arvore();
+		int tamanho = vetor.length;
+		for (int i = 0; i < tamanho; i++) {
+            arvore.insertLeaf(vetor[i]);
+        }
+		try {
+			arvore.prefixSearch();
+			arvore.infixSearch();
+			arvore.postfixSearch();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
